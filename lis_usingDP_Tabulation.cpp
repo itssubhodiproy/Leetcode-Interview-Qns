4b36a711f21fcbfe77d21f00c17fa50a6fa99c70
Subhodip Roy <75121304+Subhodip281@users.noreply.github.com>
2021-10-29 09:39:01 +0530

    Update lis_usingDP_Tabulation.cpp

--- lis_usingDP_Tabulation.cpp
+++ lis_usingDP_Tabulation.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-int main()
-{
+                                                                   <<-------------Striver's Logic------mYlogic-Downside
+int main()                                                                                                          
+{                                                                                                                  
     int n;
     cin >> n;
     int arr[n];
@@ -26,5 +26,36 @@ int main()
 
     cout << *max_element(lis, lis + n) << endl;
 
+    return 0;
+}
+
+
+#include <bits/stdc++.h>                                ---------------------->>>>>>>>> My logic <<<<<<<<<<<<<<<-------------------
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];int dp[n];for(int i=0;i<n;i++)dp[i]=1;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        int maxi = arr[i];
+        int count = 1;
+        for (int j = i; j < n; j++)
+        {
+            if (arr[j] > maxi)
+            {
+                maxi = arr[j];
+                dp[i]=dp[i]+1;
+            }
+        }
+    }
+
+    cout<<*max_element(dp,dp+n)<<endl;
     return 0;
 }

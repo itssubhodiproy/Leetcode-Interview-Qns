0230ab021b685d3eb0c35772278b6115e6b4ab4a
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2021-12-05 20:31:29 +0530

    Update Staircase_Dp.cpp

--- Staircase_Dp.cpp
+++ Staircase_Dp.cpp
@@ -2,23 +2,32 @@
 using namespace std;
 int arr[1000];
 
-int stair(int n)
-{
+// int stair(int n)                                <<<<<<<<<<<<<-------Memoization---------->>>>>>>>>>
+// {
 
-    if (arr[n] != 0)
-    {
-        return arr[n];
+//     if (arr[n] != 0)
+//     {
+//         return arr[n];
+//     }
+
+//     if (n <= 2)
+//     {
+//         arr[n] = n;
+//         return n;
+//     }
+
+//     arr[n] = stair(n - 1) + stair(n - 2);
+//     return arr[n];
+// }
+
+ int stair(int n) {                                         ///////// <<<<<<<<----------Tabulation----------->>>>>>>>>>>
+        int dp[n+1];
+        dp[0]=1; dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
     }
-
-    if (n <= 2)
-    {
-        arr[n] = n;
-        return n;
-    }
-
-    arr[n] = stair(n - 1) + stair(n - 2);
-    return arr[n];
-}
 
 int main()
 {

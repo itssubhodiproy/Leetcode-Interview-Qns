a6ba16536138915c582a51a4c0f1670bcab26dbd
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-01-12 19:48:50 +0530

    Update 236. Lowest Common Ancestor of a Binary Tree.cpp

--- 236. Lowest Common Ancestor of a Binary Tree.cpp
+++ 236. Lowest Common Ancestor of a Binary Tree.cpp
@@ -1,1 +1,41 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+                                        // My Own Code
+
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        //If I've found the value or NULL
+        if(!root || root==p || root==q){
+            return root;
+        }
+        
+        //Recursive calls will go-on and try to find the nodes
+        TreeNode* l = lowestCommonAncestor(root->left,p,q);
+        TreeNode* r = lowestCommonAncestor(root->right,p,q);
+        
+        //if We've found the left & Right node, return root;
+        if(l && r){
+            return root;
+        }
+        //If left is containing our value
+        if(l!=NULL){
+            return l;
+        }
+        //If right is containing our value
+        if(r!=NULL){
+            return r;
+        }
+        //if both are null
+        return NULL;
+    }
+};

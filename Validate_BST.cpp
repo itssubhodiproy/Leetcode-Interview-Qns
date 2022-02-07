09a57db5d014c9212a72771bf7fc525552589ec7
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-02-07 20:11:03 +0530

    Update Validate_BST.cpp

--- Validate_BST.cpp
+++ Validate_BST.cpp
@@ -41,3 +41,28 @@ public:
     return true;
     }
 };
+
+// Easiest solution came across so far
+void inorder(TreeNode* root, vector<int> &v){
+    if(!root) return;
+    inorder(root->left,v);
+    v.push_back(root->val);
+    inorder(root->right,v);
+}
+bool isThisIncreasing(vector<int> v){
+    for(int i=0;i<v.size()-1;i++){
+        if(v[i]>=v[i+1]){
+            return false;
+        }
+    }
+    return true;
+}
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        vector<int> v;
+        inorder(root,v);
+        if(isThisIncreasing(v)) return true;
+        return false;
+    }
+};

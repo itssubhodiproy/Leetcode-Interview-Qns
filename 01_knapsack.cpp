b8f46b49d22f3ba210981d79807d94fc52014477
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-02-24 09:04:50 +0530

    Update 01_knapsack.cpp

--- 01_knapsack.cpp
+++ 01_knapsack.cpp
@@ -35,3 +35,30 @@
        memset(v,-1,sizeof (v));
        return memoized(W,wt,val,n);
     }
+
+
+// Optimized Tabulation solution
+
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       int v [n+1][W+1];
+       for(int i=0;i<n+1;++i){
+           for(int j=0;j<W+1;++j){
+               if(i==0||j==0){
+                   v[i][j]=0;
+               }
+           }
+       }
+    //   Important part
+    for(int i=1;i<n+1;++i){
+        for(int j=1;j<W+1;j++){
+            if(j>=wt[i-1]){
+            v[i][j]=max( (val[i-1]+v[i-1][j-wt[i-1]]), v[i-1][j] );
+             }
+            else{
+           v[i][j]=v[i-1][j];
+            }
+        }
+    }
+    return v[n][W];
+    }

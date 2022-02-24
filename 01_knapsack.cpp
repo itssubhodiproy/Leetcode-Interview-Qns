bf95568a44933cac2dfe75ea5d97e2a8f69e9407
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-02-24 08:42:33 +0530

    Update 01_knapsack.cpp

--- 01_knapsack.cpp
+++ 01_knapsack.cpp
@@ -12,3 +12,26 @@
 //       If wt[n-1]>W, skip that element
        return knapSack(W,wt,val,n-1);    
     }
+
+
+// Optimized Memoized solution
+    int v [1005][1005];
+    
+    int memoized(int W, int wt[], int val[], int n){
+        if(W==0||n==0){
+           return 0;
+       }
+       if(v[n][W]!=-1){
+           return v[n][W];
+       }
+       if(W>=wt[n-1]){
+           return v[n][W]=max( (val[n-1]+memoized(W-wt[n-1],wt,val,n-1)),memoized(W,wt,val,n-1) );
+       }
+       return v[n][W]=memoized(W,wt,val,n-1);
+    }
+
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       memset(v,-1,sizeof (v));
+       return memoized(W,wt,val,n);
+    }

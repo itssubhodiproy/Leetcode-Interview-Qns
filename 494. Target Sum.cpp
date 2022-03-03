e2c292c59145e8950472b107d90d832159cfa6bf
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-03-03 20:27:45 +0530

    Create 494. Target Sum.cpp

---
+++ 494. Target Sum.cpp
@@ -0,0 +1,48 @@
+int countSubsets(vector<int>& nums, int n, int M)
+    {
+        int t[n + 1][M + 1];
+        
+       for(int i = 0; i <= n; i++)
+        {
+            for(int j = 0; j <= M; j++)
+            {
+                if(i == 0)
+                    t[i][j] = 0;
+                if(j == 0)
+                    t[i][j] = 1;
+            }
+        }
+        
+        //t[0][0] = 1;
+        
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j <= M; j++)
+            {
+                if(nums[i - 1] <= j)
+                 t[i][j] = t[i - 1][j - nums[i - 1]] + t[i - 1][j];
+                else
+                  t[i][j] = t[i - 1][j];
+            }
+        }
+        
+        return t[n][M];  
+    }
+
+
+class Solution {
+public:
+ int findTargetSumWays(vector<int>& nums, int target)
+     {
+         target = abs(target);
+         int n = nums.size();
+         int sum = 0;
+         for(int i = 0; i < n; i++)
+             sum += nums[i];
+         
+        int M = (sum + target)/2;
+        if(sum < target || (sum + target) % 2 != 0)
+            return 0;
+         return countSubsets(nums, n, M);
+     }
+};

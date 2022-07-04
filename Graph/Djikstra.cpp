2202f8cd301f16497c73a8399bcc8e22cc6f9335
Subhodip Roy <75121304+subh-23@users.noreply.github.com>
2022-07-04 10:25:31 +0530

    Update Djikstra.cpp

--- Graph/Djikstra.cpp
+++ Graph/Djikstra.cpp
@@ -1,17 +1,13 @@
 priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq; 
     vector<int> dis(n + 1, INT_MAX);// 1-indexed array for calculating shortest paths;
-
     dis[source] = 0;
     pq.push(make_pair(0, source));
-    while (!pq.empty())
-    {
-        int node = pq.top().second;
-        for (auto it : adj[node])
-        {
+    while (!pq.empty()){
+        int node = pq.top().second;pq.pop();
+        for (auto it : adj[node]){
             int nextNode = it.first;
             int wt = it.second;
-            if (dis[nextNode] > dis[node] + wt)
-            {
+            if (dis[nextNode] > dis[node] + wt){
                 dis[nextNode] = dis[node] + wt;
                 pq.push({dis[nextNode], nextNode});
             }
